f3712de398ad4802b8e60ce3e9a969dd9b19aefe
Guilherme Honda <guilherme.honda@meseems.com.br>
2022-08-20 12:24:43 -0300

    main

---
+++ ClassLibrary1/JokenPoTest.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+using Teste;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class JokenPoTest
+    {
+        [SetUp]
+        public void Init()
+        {
+            jokenpo = new JokenPo();
+        }
+
+        [Test]
+        public void When_Play_Rock_and_Scissor_Then_Rock_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Rock, JokenPoEnum.Scissor);
+            Assert.AreEqual(JokenPoEnum.Rock.ToString(), result);
+        }
+
+        [Test]
+        public void When_Play_Rock_and_Paper_Then_Paper_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Rock, JokenPoEnum.Paper);
+            Assert.AreEqual(JokenPoEnum.Paper.ToString(), result);
+        }
+
+        [Test]
+        public void When_Play_Rock_and_Rock_Then_Draw()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Rock, JokenPoEnum.Rock);
+            Assert.AreEqual("Draw", result);
+        }
+
+        [Test]
+        public void When_Play_Scissor_and_Rock_Then_Rock_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Scissor, JokenPoEnum.Rock);
+            Assert.AreEqual(JokenPoEnum.Rock.ToString(), result);
+        }
+
+        [Test]
+        public void When_Play_Scissor_and_Paper_Then_Scissor_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Scissor, JokenPoEnum.Paper);
+            Assert.AreEqual(JokenPoEnum.Scissor.ToString(), result);
+        }
+
+        [Test]
+        public void When_Play_Scissor_and_Scissor_Then_Draw()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Scissor, JokenPoEnum.Scissor);
+            Assert.AreEqual("Draw", result);
+        }
+
+        [Test]
+        public void When_Play_Paper_and_Paper_Then_Draw()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Paper, JokenPoEnum.Paper);
+            Assert.AreEqual("Draw", result);
+        }
+
+        [Test]
+        public void When_Play_Paper_and_Scissor_Then_Scissor_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Paper, JokenPoEnum.Scissor);
+            Assert.AreEqual(JokenPoEnum.Scissor.ToString(), result);
+        }
+
+        [Test]
+        public void When_Play_Paper_and_Rock_Then_Paper_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Paper, JokenPoEnum.Rock);
+            Assert.AreEqual(JokenPoEnum.Paper.ToString(), result);
+        }
+
+        [Test]
+        public void When_Play_Paper_and_Pencil_Then_Pencil_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Paper, JokenPoEnum.Pencil);
+            Assert.AreEqual(JokenPoEnum.Pencil.ToString(), result);
+        }
+
+        [Test]
+        public void When_Play_Pencil_and_Rock_Then_Pencil_Wins()
+        {
+            var result = jokenpo.Play(JokenPoEnum.Pencil, JokenPoEnum.Rock);
+            Assert.AreEqual(JokenPoEnum.Pencil.ToString(), result);
+        }
+
+        private JokenPo jokenpo;
+    }
+}
---
+++ Teste/JokenPo.cs
@@ -0,0 +1,40 @@
+﻿namespace Teste
+{
+    public class JokenPo
+    {
+        public string Play(JokenPoEnum player1, JokenPoEnum player2)
+        {
+            if (player1 == player2)
+                return "Draw";
+
+            if (player1 == JokenPoEnum.Pencil || player2 == JokenPoEnum.Pencil)
+                return JokenPoEnum.Pencil.ToString();
+
+            switch (player1, player2)
+            {
+                case (JokenPoEnum.Paper, JokenPoEnum.Scissor):
+                    return JokenPoEnum.Scissor.ToString();
+                case (JokenPoEnum.Paper, JokenPoEnum.Rock):
+                    return JokenPoEnum.Paper.ToString();
+                case (JokenPoEnum.Scissor, JokenPoEnum.Paper):
+                    return JokenPoEnum.Scissor.ToString();
+                case (JokenPoEnum.Scissor, JokenPoEnum.Rock):
+                    return JokenPoEnum.Rock.ToString();
+                case (JokenPoEnum.Rock, JokenPoEnum.Paper):
+                    return JokenPoEnum.Paper.ToString();
+                case (JokenPoEnum.Rock, JokenPoEnum.Scissor):
+                    return JokenPoEnum.Rock.ToString();
+                default:
+                    throw new System.Exception("Jogada inválida");
+            }
+        }
+    }
+
+    public enum JokenPoEnum
+    {
+        Paper,
+        Scissor,
+        Rock,
+        Pencil
+    }
+}
